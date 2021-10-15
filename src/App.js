46ce03685618ace733b035946064893271db8562
Esteban <estebanvargas10@gmail.com>
2021-10-14 21:35:30 -0600

    Temp Landing Page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-210297258-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Música de Costa Rica </h1>
+        <h4>(Proyecto en desarrollo)</h4>
+        Siguenos en Instagram!
+        {/* {'\n'} */}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/MusicaDeCostaRica/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          @MusicaDeCostaRica
         </a>
       </header>
     </div>
