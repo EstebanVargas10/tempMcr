805e66dd7d8f7ab1296f33f15217c49c1e78090a
Esteban <estebanvargas10@gmail.com>
2023-01-08 18:43:13 -0600

    Donations added

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 const express = require('express');
 const app = express();
-var git = require('git-rev-sync');
 
 app.listen(3000, () => {
   console.log('Application started and Listening on port 3000');
 });
 
-const gitHash = git.short();
-console.log(`Git hash: ${gitHash}`);
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/pages/index.html');
 });
 
-app.get('/registro', (req, res) => {
-  res.sendFile(__dirname + '/pages/registro.html');
+app.get('/buymeacoffee', (req, res) => {
+  res.sendFile(__dirname + '/pages/buymeacoffee.html');
+});
+
+app.get('/donaciones', (req, res) => {
+  res.sendFile(__dirname + '/pages/donaciones.html');
+});
+
+app.get('/insta', (req, res) => {
+  res.sendFile(__dirname + '/pages/insta.html');
+});
+
+app.get('/patreon', (req, res) => {
+  res.sendFile(__dirname + '/pages/patreon.html');
 });
 
 app.get('/playlists', (req, res) => {
   res.sendFile(__dirname + '/pages/playlists.html');
 });
 
-app.get('/insta', (req, res) => {
-  res.sendFile(__dirname + '/pages/insta.html');
+app.get('/registro', (req, res) => {
+  res.sendFile(__dirname + '/pages/registro.html');
+});
+
+app.get('/sinpe', (req, res) => {
+  res.sendFile(__dirname + '/pages/sinpe.html');
 });
 
 // 404
